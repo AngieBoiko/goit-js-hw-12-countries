071d83e5dd5adb91264375d2e1acd3febf252fc0
Anzhela Boiko <boikoanzhela@gmail.com>
2021-07-01 22:45:24 +0300

    last version with error

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
-import API from './js/fetchCountries.js';
-import countriesList from './templates/countriesList.hbs';
-import countryCard from './templates/countryCard.hbs';
+import fetchCountries from './js/fetchCountries.js';
+import list from './templates/countriesList.hbs';
+import card from './templates/countryCard.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+import { alert, notice, info, success, error } from '@pnotify/core';
 
+  
 const refs = {
     input: document.querySelector('[data-action="input"]'),
     container:document.querySelector('.container'),
 }
 const debounce = require('lodash.debounce');
+let searchQuery = '';
 
 refs.input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
-    const inputItem = e.target;
-    const searchQuery = inputItem.value.trim();
-    API.fetchCountries(searchQuery)
-        .then((data) => {
-            if (data.length >= 2 && data.length <= 10) {
-                renderList(data);
-            }
-        })
-        .then((data) => {
-            if (data.length < 2) {
-                renderCard(data);
-            };
-            
-        });
+    searchQuery = e.target.value;
+    fetchCountries(searchQuery).then(render).catch(onCatch);
+    
 };
 
-function renderList(countries) {
-    const markup = countriesList(countries);
-    refs.container.innerHTML = markup;
+
+function render(countries) {
+    if (countries.length === 1) {
+        renderCountry(countries);     
+    } else if (countries.length > 1 && countries.length <= 10) {
+        renderCountries(countries);        
+    } else if (countries.length > 10) {
+        alert({
+        text: "Too many countries!Please enter more specific!"
+        });
+    }
+}
+
+function renderCountry(countries) {
+    countries.forEach(country => {
+        refs.container.innerHTML = card(country);
+        refs.input.value = '';
+    });    
 };
-function renderCard(country) {
-    const markupCard = countryCard(country);
-    refs.container.innerHTML = markupCard;
+
+function renderCountries(countries) {
+    const countriesArray = countries.map(country => country.name);
+    refs.container.innerHTML = list(countriesArray);
+};
+
+function onCatch (er) {
+    error({
+        text:`${er}`
+    })
 };
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,12 @@
-const BASE_URL = `https://restcountries.eu/rest/v2/name`;
+const BASE_URL = `https://restcountries.eu/rest/v2`;
 
-function fetchCountries(searchQuery) {
-    return fetch(`${BASE_URL}/${searchQuery}`).then(response=>response.json())
+export default function fetchCountries(searchQuery) {
+    return fetch(`${BASE_URL}/name/${searchQuery}`)
+        .then(response => {
+        if (!response.ok){
+            throw Error(`Country does not exist!`);
+        }
+        return response.json();
+      })
 };
 
-export default { fetchCountries };
