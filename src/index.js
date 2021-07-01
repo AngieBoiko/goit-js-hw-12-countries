b56e48f635894ef10f14a732117f23f5bb9a1674
Anzhela Boiko <boikoanzhela@gmail.com>
2021-07-02 01:14:23 +0300

    fix handlebar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 import fetchCountries from './js/fetchCountries.js';
 import list from './templates/countriesList.hbs';
 import card from './templates/countryCard.hbs';
@@ -5,16 +6,19 @@ import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/Material.css';
 import { alert, notice, info, success, error } from '@pnotify/core';
 
+
+
   
 const refs = {
     input: document.querySelector('[data-action="input"]'),
-    container:document.querySelector('.container'),
+    container: document.querySelector('.container'),
+    resetBtn:document.querySelector('[data-action="reset-button"]'),
 }
 const debounce = require('lodash.debounce');
 let searchQuery = '';
 
 refs.input.addEventListener('input', debounce(onSearch, 500));
-
+refs.resetBtn.addEventListener('click', onReset);
 
 function onSearch(e) {
      if(!e.target.value) {
@@ -38,20 +42,34 @@ function render(countries) {
     }
 }
 
-function renderCountry(countries) {
-    countries.forEach(country => {
-        refs.container.innerHTML = card(country);
-        refs.input.value = '';
-    });    
+function renderCountry(country) {
+    clearContent();
+    const marupCard = card(country);
+        refs.container.innerHTML =marupCard ;
+    
+   
 };
 
 function renderCountries(countries) {
-    const countriesArray = countries.map(country => country.name);
-    refs.container.innerHTML = list(countriesArray);
+    clearContent();
+    const markupList=list(countries);
+    refs.container.innerHTML = markupList;
 };
 
-function onCatch (er) {
+function onCatch(er) {
+    clearContent();
     error({
         text:`${er}`
     })
 };
+
+    function clearContent() {
+    refs.container.innerHTML=''
+};
+function onReset(event) {
+    event.preventDefault();
+    clearContent()
+    refs.input.value=''
+
+
+}
